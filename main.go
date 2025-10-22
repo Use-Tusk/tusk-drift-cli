88780ce3c1d858e2d3b42a93943a88b04327d78b
JY Tan <jy8230@gmail.com>
2025-10-22 02:25:54 -0700

    chore: use prod auth0 (#28)

--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import (
 	"os"
 
 	"github.com/Use-Tusk/tusk-drift-cli/cmd"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	_ = godotenv.Load()
+
 	if err := cmd.Execute(); err != nil {
 		os.Exit(1)
 	}
