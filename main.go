6b042c20dd71b8cf4c7bc9e06ec2789507ca9dd0
JY Tan <jy8230@gmail.com>
2025-11-18 13:51:25 +0800

    Commit

--- main.go
+++ main.go
@@ -1,20 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/Use-Tusk/tusk-drift-cli/cmd"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil && !os.IsNotExist(err) {
-		fmt.Printf("Error loading .env file: %v\n", err)
-		os.Exit(1)
-	}
-
 	if err := cmd.Execute(); err != nil {
 		os.Exit(1)
 	}
